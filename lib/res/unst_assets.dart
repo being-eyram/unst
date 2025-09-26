b723bf501574cc4b8decf6bc5484c53645f15ea2
Eyram Hlorgbe <eyram.hlorgbe@hubtel.com>
2025-09-26 23:06:04 +0000

    Add icon and color resource classes

---
+++ lib/res/unst_assets.dart
@@ -0,0 +1,11 @@
+class UnstAssets{
+  const UnstAssets._();
+
+  static const String  _dir = "assets/icons";
+  static String icHome = "$_dir/ic_home.svg";
+  static String icHomeBold = "$_dir/ic_home_bold.svg";
+  static String icSend = "$_dir/ic_send.svg";
+  static String icSendBold = "$_dir/ic_send_bold.svg";
+  static String icWardrobe = "$_dir/ic_wardrobe.svg";
+  static String icWardrobeBold = "$_dir/ic_wardrobe_bold.svg";
+}
--- lib/res/unst_colors.dart
+++ lib/res/unst_colors.dart
@@ -6,4 +6,6 @@ class UnstColors{
   const UnstColors._();
   static Color borderUnselected = Color(0xFFCBCBCB);
   static Color borderSelected = Colors.black;
+  static Color appBackground = Color(0xFFE9DFFC);
+  static Color bottomNavBackground = Color(0xFFE9DFFC);
 }
